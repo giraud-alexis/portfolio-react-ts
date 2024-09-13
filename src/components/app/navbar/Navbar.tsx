3d2ec6f7c98b0cc74bbaff899f96c885f341ae1b
giraud-alexis <alexis33190@gmail.com>
2024-09-13 23:23:51 +0200

    responsive terminé

--- src/components/app/navbar/Navbar.tsx
+++ src/components/app/navbar/Navbar.tsx
@@ -1,8 +1,45 @@
+import { useEffect, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
 import note from '../../../assets/img/notes-de-musique.png'
+import musique from '../../../assets/music/Emmetelle-Awake.mp3';
+import disc from '../../../assets/img/disc.png'
 import './navbar.scss'
 
 const Navbar = () => {
+    const audioRef = useRef<HTMLAudioElement | null>(null);
+
+    useEffect(() => {
+        const handleClick = () => {
+            if (audioRef.current) {
+                if (audioRef.current.paused) {
+                    audioRef.current.play();
+                    const imgMusique = document.getElementById('note');
+                    if (imgMusique) {
+                        imgMusique.classList.add('playing');
+                    }
+                } else {
+                    audioRef.current.pause();
+                    audioRef.current.currentTime = 0;
+                    const imgMusique = document.getElementById('note');
+                    if (imgMusique) {
+                        imgMusique.classList.remove('playing');
+                    }
+                }
+            }
+        };
+
+        const imgMusique = document.getElementById('note');
+        if (imgMusique) {
+            imgMusique.addEventListener('click', handleClick);
+        }
+
+        return () => {
+            if (imgMusique) {
+                imgMusique.removeEventListener('click', handleClick);
+            }
+        };
+    }, []);
+
     return (
         <div className="navbar">
             <nav>
@@ -17,7 +54,8 @@ const Navbar = () => {
                         <NavLink to={'/about'}>About</NavLink>
                     </li>
                 </ul>
-                <img src={note} alt="" />
+                <img id='note' src={disc} alt="" />
+                <audio ref={audioRef} src={musique} />
             </nav>
         </div>
     );
